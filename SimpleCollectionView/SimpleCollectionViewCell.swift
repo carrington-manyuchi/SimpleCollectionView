b6533ddc8934c0484712508921b25616281545f3
Carrington <Manyuchi>
2023-02-26 11:36:00 +0200

    Added cell layout on storyboard ADDED UICollectionViewCell Controller

---
+++ SimpleCollectionView/SimpleCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleCollectionViewCell.swift
+//  SimpleCollectionView
+//
+//  Created by Carrington Tafadzwa Manyuchi on 2023/02/26.
+//
+
+import UIKit
+
+class SimpleCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+}
